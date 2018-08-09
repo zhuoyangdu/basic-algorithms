efb9fa06c320c5ff5c67f5a6eb923ce6eaee9aae
Zhuoyang Du <zhuoyang.dzy@cainiao.com>
2018-08-09 17:06:37 +0800

    Init.

---
+++ common/list_node.h
@@ -0,0 +1,14 @@
+//
+// Created by zy on 18-8-9.
+//
+
+#ifndef CODING_LIST_NODE_H
+#define CODING_LIST_NODE_H
+
+struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode(int x) : val(x), next(NULL) {}
+};
+
+#endif //CODING_LIST_NODE_H
---
+++ sort/insert_sort.h
@@ -0,0 +1,22 @@
+//
+// Created by zy on 18-8-9.
+//
+
+#ifndef CODING_INSERT_SORT_H
+#define CODING_INSERT_SORT_H
+
+#include "../common/list_node.h"
+
+// 插入排序
+// 在要排序的一组数中，假定前n-1个数已经排好序，现在将第n个数插到前面的有序数列中，使得这n个数也是排好顺序的。
+// 如此反复循环，直到全部排好顺序。
+
+static ListNode* insertionSortList(ListNode* head) {
+    ListNode* head0 = head;
+    ListNode* current = head0;
+    while (current!=NULL){
+        
+    }
+}
+
+#endif //CODING_INSERT_SORT_H
